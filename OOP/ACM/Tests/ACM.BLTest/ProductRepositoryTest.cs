0ab5b2051a2169de35b205941d9d52ea044286b5
patricK09 <patrickhughes013@gmail.com>
2019-07-21 13:35:32 -0500

    C# Projects
    Accidentally deleted my stuff. Oops

---
+++ OOP/ACM/Tests/ACM.BLTest/ProductRepositoryTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveProductDisplayTest()
+        {
+            //arrange
+            var productRepository = new ProductRepository();
+            var expected = new Product(2)
+            {
+                ProductName = "Sunflowers",
+                ProductDescription = "Assorted size set of 4 bright yellow mini sunflowers",
+                CurrentPrice = 15.96M
+            };
+
+            //act
+            var actual = productRepository.Retrieve(2);
+
+            //assert
+            Assert.AreEqual(expected.ProductName, actual.ProductName);
+            Assert.AreEqual(expected.ProductDescription, actual.ProductDescription);
+            Assert.AreEqual(expected.CurrentPrice, actual.CurrentPrice);
+        }
+
+        [TestMethod]
+        public void SaveTestValid()
+        {
+            //arrange
+            var productRepository = new ProductRepository();
+            var updatedProduct = new Product(2)
+            {
+                ProductName = "Sunflowers",
+                ProductDescription = "Assorted size set of 4 bright yellow mini sunflowers",
+                CurrentPrice = 18M,
+                HasChanges = true
+            };
+
+            //act
+            var actual = productRepository.Save(updatedProduct);
+
+            //assert
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void SaveTestMissingPrice()
+        {
+            //arrange
+            var productRepository = new ProductRepository();
+            var updatedProduct = new Product(2)
+            {
+                ProductName = "Sunflowers",
+                ProductDescription = "Assorted size set of 4 bright yellow mini sunflowers",
+                CurrentPrice = null,
+                HasChanges = true
+            };
+
+            //act
+            var actual = productRepository.Save(updatedProduct);
+
+            //assert
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
